63356f1ed36e7f1b1dfb858985d19c1f9c07cca3
AdriBarda <adri.bardaji@icloud.com>
2025-12-23 17:39:17 +0100

    Added urls/me to the actual router

--- apps/api/src/routes/urls.ts
+++ apps/api/src/routes/urls.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express'
 import { createShortUrl } from '../services/urlShortenerService'
 import { requireAuthSid } from '../middlewares/requireAuthSid'
+import { findByUserId } from '../repositories/urlRepository'
 
 export const urlsRouter = Router()
 
 urlsRouter.post('/urls', requireAuthSid, async (req, res, next) => {
   try {
     const result = await createShortUrl(req.body, req.auth!.userId)
+    return res.status(201).json(result)
+  } catch (err) {
+    return next(err)
+  }
+})
+
+urlsRouter.get('/me', requireAuthSid, async (req, res, next) => {
+  try {
+    const result = await findByUserId(req.auth!.userId)
     return res.status(201).json(result)
   } catch (err) {
     return next(err)
