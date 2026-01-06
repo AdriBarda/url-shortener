9b456a77e550a270d507cfd1087967ed534454c6
AdriBarda <adri.bardaji@icloud.com>
2026-01-06 16:10:47 +0100

    rolled back to views instead of pages

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/pages/HomeView.vue'
-import DashboardView from '@/pages/DashboardView.vue'
-import UrlDetailsView from '@/pages/UrlDetailsView.vue'
+import HomeView from '@/views/HomeView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import UrlDetailsView from '@/views/UrlDetailsView.vue'
 import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
@@ -18,12 +18,12 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../pages/AboutView.vue'),
+      component: () => import('../views/AboutView.vue'),
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/pages/LoginView.vue'),
+      component: () => import('@/views/LoginView.vue'),
     },
     {
       path: '/dashboard',
